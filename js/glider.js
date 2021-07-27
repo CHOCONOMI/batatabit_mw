672e901f136126dcf7606aff90d004bdad113a1b
Natalie Noemí Zamora Santizo <imachaoskawaii@gmail.com>
2021-07-26 22:40:50 -0600

    Update glider.js

--- js/glider.js
+++ js/glider.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.ctablas'));
 })
+
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.cplanes'));
+})

a790b3a9ca4d24c3d7bfdbdbfa24c2f1c85dfd8e
Natalie Noemí Zamora Santizo <imachaoskawaii@gmail.com>
2021-07-31 20:55:16 -0600

    Delete js directory

--- js/glider.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.ctablas'));
-})
-
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.cplanes'));
-})
